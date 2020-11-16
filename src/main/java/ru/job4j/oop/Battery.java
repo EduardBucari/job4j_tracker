0f99e0876467b45d66a5cc22ca814305c40a3fe6
Eduard Bucari <eduard.bucari@gmail.com>
2020-11-16 03:26:26 +0300

    1.7. Состояние объекта [#361027]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private  int load;
+
+    public  Battery(int energySize) {
+        this.load = energySize;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(300);
+        Battery second = new Battery(100);
+        System.out.println("first battery : " + first.load + ". second battery : " + second.load);
+        first.exchange(second);
+        System.out.println("first battery : " + first.load + ". second battery : " + second.load);
+    }
+}
+
