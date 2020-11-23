82e5daf17a19724f5d0716d82485b09855feb7ef
Eduard Bucari <eduard.bucari@gmail.com>
2020-11-24 02:49:22 +0300

    6. Date. Отображение даты. [#361031](3)

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,15 +6,13 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
 
     public static void main(String[] args) {
-        LocalDateTime created = LocalDateTime.now();
+
         Item startItem = new Item();
-            startItem.getCreated();
-        }
-        LocalDateTime created = LocalDateTime.now();
+        LocalDateTime created = startItem.getCreated();
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String createdFormat = created.format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + createdFormat);
+    }
 }
-
 
