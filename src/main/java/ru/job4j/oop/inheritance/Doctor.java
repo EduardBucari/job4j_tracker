f9e9c90099394a9c7e70556a3c6ffe8b3ee49e33
Eduard Bucari <eduard.bucari@gmail.com>
2020-11-24 18:56:17 +0300

    3. Реализация профессий в коде [#361040](2)

--- src/main/java/ru/job4j/oop/inheritance/Doctor.java
+++ src/main/java/ru/job4j/oop/inheritance/Doctor.java
@@ -1,19 +1,27 @@
 package ru.job4j.oop.inheritance;
 
 public class Doctor extends Profession {
+
     private String diplomaID;
     private String lengthOfService;
     private String nameOfTheMedicalUniversity;
 
-    public String getDiplomaID() {
-        return diplomaID;
-    }
+        public String getDiplomaID () {
+          return diplomaID;
+         }
 
-    public String getLengthOfService() {
-        return lengthOfService;
-    }
+        public String getLengthOfService () {
+            return lengthOfService;
+        }
 
-    public String getNameOfTheMedicalUniversity() {
-        return nameOfTheMedicalUniversity;
+        public String getNameOfTheMedicalUniversity () {
+            return nameOfTheMedicalUniversity;
+        }
+    public static void main(String[] args) {
+        Doctor doctor = new Doctor();
+        doctor.getName();
+        doctor.getSurname();
+        doctor.getBirthday();
+        doctor.getEducation();
     }
 }
