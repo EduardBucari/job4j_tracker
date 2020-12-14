0047b9638c0abb894164adc5289788ff72acf778
Eduard Bucari <eduard.bucari@gmail.com>
2020-12-15 02:43:40 +0300

    8. Что такое валидация? [#361048](2)

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -31,35 +31,37 @@ public class Tracker {
         return Arrays.copyOf(findByName, sizeFBN);
     }
 
-    public Item findById(int id) {
-        //  Item rsl = null;
-        //  for (int index = 0; index < size; index++) {
-        //     Item item = items[index];
-        //     if (item.getId() == id) {
-        //         rsl = item;
-        //        break;
-        //   }
-        //  }
-        // return rsl;
+  /* public Item findById(int id) {
+          Item rsl = null;
+          for (int index = 0; index < size; index++) {
+             Item item = items[index];
+             if (item.getId() == id) {
+                 rsl = item;
+                break;
+           }
+          }
+         return rsl; */
 
         // Упрощаем метод  findById
+        public Item findById(int id) {
         int index = indexOf(id);
         return index != -1 ? items[index] : null;
     }
 
-  //  public boolean replace(int id, Item item) {
-  //      int index = indexOf(id);
-  //      if (index != -1) {
-  //          item.setId(id);
-  //          items[index] = item;
-  //          return true;
-  //      }
-  //      return false;
-  //  }
+   /* public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
+            return true;
+        }
+        return false;
+    } */
 
-    @Override  //добавляем валидацию параметров
+      //добавляем валидацию параметров в метод replace();
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
+        item.setId(id);
         boolean rsl = index  != -1;
         if (rsl) {
             items[index] = item;
@@ -78,7 +80,7 @@ public class Tracker {
         return rsl;
     }
 
-    public boolean delete(int id) {
+   /* public boolean delete(int id) {
         int index = indexOf(id);
         if (index != -1) {
             int start = index + 1;
@@ -89,6 +91,23 @@ public class Tracker {
             items[size - 1] = null;
             size--;
           return true;
+        }
+        return false;
+    }  */
+
+    //добавляем валидацию параметров в метод delete();
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            int start = index + 1;
+            int distPos = index;
+            int length = size - index;
+
+            System.arraycopy(items, start, items, distPos, length);
+            items[size - 1] = null;
+            size--;
+            return rsl;
         }
         return false;
     }
