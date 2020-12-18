ad785eab4f76ee9d8a6bac5d1fc8983e52eddc47
Eduard Bucari <eduard.bucari@gmail.com>
2020-12-19 02:18:34 +0300

    (3)

---
+++ src/main/java/ru/job4j/tracker/StartNewUI.java
@@ -0,0 +1,43 @@
+package ru.job4j.tracker;
+
+import java.util.Scanner;
+
+public class StartNewUI {
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = false;
+        while (run) {
+            this.showMenu();
+            System.out.println("Select: ");
+            int select = Integer.valueOf(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Create a new Item ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(001, "");
+                tracker.add(item);
+            } else if (select == 1) {
+
+            } // +++++++++++++++++++
+        }
+    }
+        private void showMenu() {
+            System.out.println("Menu.");
+            System.out.println("0. Add new Item");
+            System.out.println("1. Show all items");
+            System.out.println("2. Edit item");
+            System.out.println("3. Delete item");
+            System.out.println("4. Find item by Id");
+            System.out.println("5. Find items by name");
+            System.out.println("6. Exit Program");
+            System.out.println("Select:");
+
+
+        }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartNewUI().init(scanner, tracker);
+    }
+}
