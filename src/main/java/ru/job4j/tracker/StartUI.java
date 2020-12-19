fcac552c547259eb06ea4ab3f10a15304dfe9a95
Eduard Bucari <eduard.bucari@gmail.com>
2020-12-19 04:02:08 +0300

    (3)

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,28 +1,69 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class StartUI {
 
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            this.showMenu();
+            System.out.println("Select: ");
+            int select = Integer.valueOf(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Create a new Item ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(0, "......");
+                tracker.add(item);
+
+            } else if (select == 1) {
+                System.out.println("=== Show all items ===");
+                System.out.print("Enter id: ");
+                System.out.println("Enter id name: ");
+                String id = scanner.nextLine();
+                tracker.findAll();
+                System.out.println();// ????????;
+
+            } else if (select == 2) {
+                System.out.println("=== Edit item ===");
+              //  tracker.replace(2, ); //  ??????
+
+            } else if (select == 3) {
+                System.out.println("=== Delete item ===");
+                tracker.delete(3);
+
+            } else if (select == 4) {
+                System.out.println("=== Find item by Id ===");
+                tracker.findById(4);
+
+            } else if (select == 5) {
+                System.out.println("=== Find items by name ===");
+                tracker.findByName("......");
+
+            } else if (select == 6) {
+                run = false;
+            }
+        }
+    }
+        private void showMenu() {
+            System.out.println("Menu.");
+            System.out.println("0. Add new Item");
+            System.out.println("1. Show all items");
+            System.out.println("2. Edit item");
+            System.out.println("3. Delete item");
+            System.out.println("4. Find item by Id");
+            System.out.println("5. Find items by name");
+            System.out.println("6. Exit Program");
+           // System.out.println("Select:");
+
+
+        }
+
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         Tracker tracker = new Tracker();
-
-        Item item = new Item (1, "test");
-
-      //  Item startItem = new Item(1103, "David");
-     //   LocalDateTime created = startItem.getCreated();
-
-     //   DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-     //   String createdFormat = created.format(formatter);
-     //   System.out.println("Текущие дата и время после форматирования: " + createdFormat);
-
-     //   System.out.println(startItem);
-
-        tracker.add(item);
-        Item rsl = tracker.findById(1);
-        System.out.println(rsl);
+        new StartUI().init(scanner, tracker);
     }
 }
-
---
+++ src/main/java/ru/job4j/tracker/StartUIpervii.java
@@ -0,0 +1,28 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUIpervii {
+
+    public static void main(String[] args) {
+
+        Tracker tracker = new Tracker();
+
+        Item item = new Item (1, "test");
+
+      //  Item startItem = new Item(1103, "David");
+     //   LocalDateTime created = startItem.getCreated();
+
+     //   DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+     //   String createdFormat = created.format(formatter);
+     //   System.out.println("Текущие дата и время после форматирования: " + createdFormat);
+
+     //   System.out.println(startItem);
+
+        tracker.add(item);
+        Item rsl = tracker.findById(1);
+        System.out.println(rsl);
+    }
+}
+
