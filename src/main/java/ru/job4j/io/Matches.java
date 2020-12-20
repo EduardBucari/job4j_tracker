1a1048acbd9b1a512886cc9fa9f38b7231f48c5a
Eduard Bucari <eduard.bucari@gmail.com>
2020-12-20 18:34:45 +0300

    2. Scanner и чтение числа из консоли. [#360971](4)

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -8,30 +8,21 @@ public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int numberOfMatchesOnTheTable = 11;
-
+        boolean player = true;
 
         while (numberOfMatchesOnTheTable > 0) {
             System.out.println("Вы можете взять одну, две или три спички: ");
             int select = Integer.valueOf(input.nextLine());
-            boolean firstPlayer;
             if (select >= 1 && select <= 3) {
-                System.out.println("Игрок N1 взял колличество спичек " + select);
+                System.out.println("Вы взяли колличество спичек: " + select);
                 numberOfMatchesOnTheTable = numberOfMatchesOnTheTable - select;
                 System.out.println("Осталось " + numberOfMatchesOnTheTable + " спичек");
-                firstPlayer = true;
-            } else {
-                firstPlayer = false;
-                System.out.println("Вы взяли не правильное колличество спичек. Повторите попытку.");
-            }
-
-            if (select >= 1 && select <= 3) {
-                System.out.println("Игрок N2 взял колличество спичек " + select);
-                numberOfMatchesOnTheTable = numberOfMatchesOnTheTable - select;
-                System.out.println("Осталось " + numberOfMatchesOnTheTable + " спичек");
-            } else {
-                System.out.println("Вы взяли не правильное колличество спичек. Повторите попытку.");
+                String name = player ? "Игрок N1" : "Игрок N2";
+                player = !player;  // Конструкция замены играков.
             }
         }
-
+        if (numberOfMatchesOnTheTable == 0) {
+            System.out.println("ВЫ ПОБЕДИТЕЛЬ!"); // Контрукция вывода победителя.
+        }
     }
 }
