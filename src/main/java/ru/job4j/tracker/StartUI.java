5cafcd4f42ab1e118cfa30ba7e08c65572921fea
Eduard Bucari <eduard.bucari@gmail.com>
2020-12-21 18:44:22 +0300

    2.1. Реализация класса StartUI [#360982](3)

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -15,12 +15,11 @@ public class StartUI {
                 System.out.println("=== Create a new Item ===");
                 System.out.print("Enter name: ");
                 String name = scanner.nextLine();
-                Item item = new Item(0, "......");
+                Item item = new Item(0, name);
                 tracker.add(item);
 
             } else if (select == 1) {
                 System.out.println("=== Show all items ===");
-                tracker.findAll();
                 Item[] rsl = tracker.findAll();//Результат вызова поместить в переменную - это массив элементов класса Item;
                 for (int i = 0; i <rsl.length; i++) {
                     System.out.println(rsl[i]);//  Выводим в консоль все элементы массива;
@@ -32,7 +31,7 @@ public class StartUI {
                 int id = Integer.parseInt(scanner.nextLine()); //Получить id заявки, которую мы будем изменять, через объект scanner;
                 String idName = scanner.nextLine();//Получить имя заявки на которую мы будем заменять через объект scanner;
                 Item item = new Item(id, idName); //Создаем объект класса Item
-                tracker.replace(id, item); //Вызвать метод replace у объекта tracker
+                                                 //Вызвать метод replace у объекта tracker
                 boolean rsl = tracker.replace(id, item);  //Валидация.
                 if (rsl = true) {  //если true то выводим строку об успешности операции, иначе - ошибка замены.
                     System.out.println("Замена заявки прошла успешно!");
@@ -44,7 +43,7 @@ public class StartUI {
             } else if (select == 3) {
                 System.out.println("=== Delete item ===");
                int id = Integer.parseInt(scanner.nextLine()); // Получить id заявки, которую мы будем удалять, через объект scanner;
-                tracker.delete(id); // Вызвать метод delete у объекта tracker, в него передать id
+                                                 // Вызвать метод delete у объекта tracker, в него передать id
                boolean rsl = tracker.delete(id); //Результат работы метода delete необходимо валидировать
                 if (rsl = true) {  //если true то выводим строку об успешности операции, иначе - ошибка удаления.
                     System.out.println("Удаление заявки прошло успешно!");
@@ -55,8 +54,8 @@ public class StartUI {
             } else if (select == 4) {
                 System.out.println("=== Find item by Id ===");
                 int id = Integer.parseInt(scanner.nextLine());// Получить id заявки, которую мы будем искать в хранилище, через объект scanner;
-                tracker.findById(id);// Вызвать метод findById() у объекта tracker, в него передать id
-               Item item = tracker.findById(id);//Результат вызова метода поместить в переменную - это будет объект класса Item;
+                                                 // Вызвать метод findById() у объекта tracker, в него передать id
+                                       //Результат вызова метода поместить в переменную - это будет объект класса Item;
                Item rsl = tracker.findById(id);//Результат работы метода findById() необходимо валидировать, поскольку метод может вернуть null;
                if (rsl != null) { //Если заявка не равна null -  мы печатаем в консоль заявку, иначе - фразу: "Заявка с таким id не найдена".
                    System.out.println(rsl);
@@ -66,14 +65,16 @@ public class StartUI {
 
             } else if (select == 5) {
                 System.out.println("=== Find items by name ===");
-               // Получить имя заявок, которые мы будем искать, через объект scanner
-                tracker.findByName("...."); // Вызвать метод findByName() у объекта tracker, в него передать имя из пункта 1
-               Item[] rslArray = tracker.findByName("....");// Результат вызова метода поместить в переменную - это будет массив объектов класса Item;
-               Item[] rsl = tracker.findByName("....");// Результат работы метода findByName() необходимо валидировать, поскольку метод может вернуть пустой массив;
-               if (rsl.length > 0) {
-                   System.out.println(rsl);// Если длина массива больше 0 - выводим массив в соответствии с примечанием 1, иначе фразу - "Заявки с таким именем не найдены".
-               } else {
-                   System.out.println("Заявки с таким именем не найдены");
+                String orderNames = scanner.nextLine(); // Получить имя заявок, которые мы будем искать, через объект scanner
+                                       // Вызвать метод findByName() у объекта tracker, в него передать имя из пункта 1
+                              // Результат вызова метода поместить в переменную - это будет массив объектов класса Item;
+               Item[] rsl = tracker.findByName(orderNames);// Результат работы метода findByName() необходимо валидировать, поскольку метод может вернуть пустой массив;
+               for (int i = 0; i < rsl.length; i++) {
+                   if (rsl.length > 0) {
+                       System.out.println(rsl[i]);// Если длина массива больше 0 - выводим массив в соответствии с примечанием 1, иначе фразу - "Заявки с таким именем не найдены".
+                   } else {
+                       System.out.println("Заявки с таким именем не найдены");
+                   }
                }
 
             } else if (select == 6) {
