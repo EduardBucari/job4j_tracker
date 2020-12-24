851194612a77b651a6b0530fce2178971a3a5b14
Eduard Bucari <eduard.bucari@gmail.com>
2020-12-24 04:56:29 +0300

    4.1. Разрыв зависимости StartUI от Scanner. [#360976]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,20 +1,28 @@
 package ru.job4j.tracker;
 
-import java.util.Arrays;
-import java.util.Scanner;
+//Если класс зависит от внешних ресурсов (системы ввода-вывода, баз данных, веб-сервисов),
+// то эти зависимости нужно разрывать.
+//1.Удаляем строчку с import java.util.Scanner.
+//import java.util.Scanner;
 
 public class StartUI {
-
-    public void init(Scanner scanner, Tracker tracker) {
+      //2.Вместо объявленныx переменных Scanner scanner написать Input input.
+ //   public void init(Scanner scanner, Tracker tracker) {
+        public void init(Input input, Tracker tracker) {
+       //Вместо вызова scanner.nextLine() написать input.askStr(msg), где msg - это сообщение,
+            // которое вы хотели бы вывести пользователю перед его вводом.
         boolean run = true;
         while (run) {
             this.showMenu();
             System.out.println("Select: ");
-            int select = Integer.valueOf(scanner.nextLine());
+          //  int select = Integer.valueOf(scanner.nextLine());
+            int select = Integer.valueOf(input.askStr(msg));
             if (select == 0) {
                 System.out.println("=== Create a new Item ===");
                 System.out.print("Enter name: ");
-                String name = scanner.nextLine();
+
+               // String name = scanner.nextLine();
+                String name = input.askStr(msg);
                 Item item = new Item(0, name);
                 tracker.add(item);
 
@@ -28,8 +36,10 @@ public class StartUI {
                 System.out.println("=== Edit item ===");
                 System.out.print("Enter id: ");
                 System.out.println("Enter id name: ");
-                int id = Integer.parseInt(scanner.nextLine()); //Получить id заявки, которую мы будем изменять, через объект scanner;
-                String idName = scanner.nextLine();//Получить имя заявки на которую мы будем заменять через объект scanner;
+               // int id = Integer.parseInt(scanner.nextLine()); //Получить id заявки, которую мы будем изменять, через объект scanner;
+               // String idName = scanner.nextLine();//Получить имя заявки на которую мы будем заменять через объект scanner;
+                int id = Integer.parseInt(input.askStr(msg));
+                String idName = input.askStr(msg);
                 Item item = new Item(id, idName); //Создаем объект класса Item
                                                  //Вызвать метод replace у объекта tracker
                 boolean rsl = tracker.replace(id, item);  //Валидация.
@@ -42,9 +52,10 @@ public class StartUI {
 
             } else if (select == 3) {
                 System.out.println("=== Delete item ===");
-               int id = Integer.parseInt(scanner.nextLine()); // Получить id заявки, которую мы будем удалять, через объект scanner;
+              // int id = Integer.parseInt(scanner.nextLine()); // Получить id заявки, которую мы будем удалять, через объект scanner;
                                                  // Вызвать метод delete у объекта tracker, в него передать id
-               boolean rsl = tracker.delete(id); //Результат работы метода delete необходимо валидировать
+                int id = Integer.parseInt(input.askStr(msg));
+                boolean rsl = tracker.delete(id); //Результат работы метода delete необходимо валидировать
                 if (rsl) {  //если true то выводим строку об успешности операции, иначе - ошибка удаления.
                     System.out.println("Удаление заявки прошло успешно!");
                 } else {
@@ -53,10 +64,11 @@ public class StartUI {
 
             } else if (select == 4) {
                 System.out.println("=== Find item by Id ===");
-                int id = Integer.parseInt(scanner.nextLine());// Получить id заявки, которую мы будем искать в хранилище, через объект scanner;
+               // int id = Integer.parseInt(scanner.nextLine());// Получить id заявки, которую мы будем искать в хранилище, через объект scanner;
                                                  // Вызвать метод findById() у объекта tracker, в него передать id
                                        //Результат вызова метода поместить в переменную - это будет объект класса Item;
-               Item rsl = tracker.findById(id);//Результат работы метода findById() необходимо валидировать, поскольку метод может вернуть null;
+                int id = Integer.parseInt(input.askStr(msg));
+                Item rsl = tracker.findById(id);//Результат работы метода findById() необходимо валидировать, поскольку метод может вернуть null;
                if (rsl != null) { //Если заявка не равна null -  мы печатаем в консоль заявку, иначе - фразу: "Заявка с таким id не найдена".
                    System.out.println(rsl);
                } else {
@@ -65,10 +77,11 @@ public class StartUI {
 
             } else if (select == 5) {
                 System.out.println("=== Find items by name ===");
-                String orderNames = scanner.nextLine(); // Получить имя заявок, которые мы будем искать, через объект scanner
+               // String orderNames = scanner.nextLine(); // Получить имя заявок, которые мы будем искать, через объект scanner
                                        // Вызвать метод findByName() у объекта tracker, в него передать имя из пункта 1
                               // Результат вызова метода поместить в переменную - это будет массив объектов класса Item;
-               Item[] rsl = tracker.findByName(orderNames);// Результат работы метода findByName() необходимо валидировать, поскольку метод может вернуть пустой массив;
+                String orderNames = input.askStr(msg);
+                Item[] rsl = tracker.findByName(orderNames);// Результат работы метода findByName() необходимо валидировать, поскольку метод может вернуть пустой массив;
                 if (rsl.length > 0) {
                     for (int i = 0; i < rsl.length; i++) {
                         System.out.println(rsl[i]);// Если длина массива больше 0 - выводим массив в соответствии с примечанием 1, иначе фразу - "Заявки с таким именем не найдены".
@@ -91,14 +104,18 @@ public class StartUI {
             System.out.println("4. Find item by Id");
             System.out.println("5. Find items by name");
             System.out.println("6. Exit Program");
-           // System.out.println("Select:");
-
-
         }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+       //3. Что бы разорвать зависимости, нужно в методе main вместо объекта new Scanner
+        // прописать объект реализации интерфейса Input.
+        //сделаем новый класс, который реализует интерфейс Input и который внутри будет работать с классом Scanner.
+        //Создадим класс ru.job4j.tracker.ConsoleInput.
+
+       // Scanner scanner = new Scanner(System.in);
+        Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
-        new StartUI().init(scanner, tracker);
+        //new StartUI().init(scanner, tracker);
+        new StartUI().init(input, tracker);
     }
 }
