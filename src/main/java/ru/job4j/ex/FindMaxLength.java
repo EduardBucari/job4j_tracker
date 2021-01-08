b4d6694261dc38357f2922be6ca4b472d9411fef
Eduard Bucari <eduard.bucari@gmail.com>
2021-01-08 19:04:50 +0300

    0.1. java.lang.NullPointerException [#360989](2)

--- src/main/java/ru/job4j/ex/FindMaxLength.java
+++ src/main/java/ru/job4j/ex/FindMaxLength.java
@@ -4,13 +4,14 @@ public class FindMaxLength {
     public static void main(String[] args) {
         String[] shops = {"Ebay", null, "Amazon", null, "Ozon"};
         int max = 0;
-        for (int index  = 0; index < shops.length; index++) {
+        for (int index = 0; index < shops.length; index++) {
             String el = shops[index];
-          //if (el.length() > max) {
             if (el != null) {
-                max = el.length();
+                if (el.length() > max) {
+                    max = el.length();
+                }
             }
         }
-        System.out.println("Max length : " + max);
+            System.out.println("Max length : " + max);
     }
 }
