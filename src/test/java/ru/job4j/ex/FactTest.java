81dc79662f9fab4bfd24b5261e682e4c4c4dacb7
Eduard Bucari <eduard.bucari@gmail.com>
2021-01-10 22:39:56 +0300

    0.3. Тестирование исключений с junit [#360991]

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,11 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+public class FactTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void whenN_les0ThenFinish() {
+        Fact.calc(-2);
+    }
+
+}
