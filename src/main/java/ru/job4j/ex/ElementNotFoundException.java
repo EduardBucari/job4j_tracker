9a9d4bb38d11281a6eb22fd93c4926f0c285df65
Eduard Bucari <eduard.bucari@gmail.com>
2021-01-11 02:30:22 +0300

    0.4. Пользовательские исключения. [#360992]

---
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -0,0 +1,9 @@
+package ru.job4j.ex;
+
+import java.security.PublicKey;
+
+public class ElementNotFoundException extends Exception {
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) { //метод поиска индекса элемента в строковом массиве.
+        int rsl = -1;
+          for ( ? : ?) {
+
+          }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            indexOf(" ", " ");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
