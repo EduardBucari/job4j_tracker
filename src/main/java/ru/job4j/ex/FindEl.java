d95bcb8c0891800e7428079978b48eeddcf1f850
Eduard Bucari <eduard.bucari@gmail.com>
2021-01-12 01:49:47 +0300

    0.4. Пользовательские исключения. [#360992](2)

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -3,8 +3,11 @@ package ru.job4j.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) { //метод поиска индекса элемента в строковом массиве.
         int rsl = -1;
-          for ( ? : ?) {
-
+          for (String val : value) {
+             if (value[index] == key) {
+                 rsl = index;
+                 break;
+             }
           }
         return rsl;
     }
