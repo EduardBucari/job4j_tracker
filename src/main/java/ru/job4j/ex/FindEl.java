74a4d366b8ba6643d7824e841a47e3c6a4b54c37
Eduard Bucari <eduard.bucari@gmail.com>
2021-01-14 02:59:08 +0300

    0.4. Пользовательские исключения. [#360992](4)

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,16 +4,16 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException { //метод поиска индекса элемента в строковом массиве.
         int rsl = -1;
           for (int index = 0; index < value.length; index++) {
-             if (value[index] == null) {
-                 throw new ElementNotFoundException("The element is not found.");
+             if (value[index].equals(key)) {
+                 return index;
              }
           }
-        return rsl;
+        throw new ElementNotFoundException("The element is not found.");
     }
 
     public static void main(String[] args) {
         try {
-            indexOf("", "");
+            indexOf("?", "?");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
