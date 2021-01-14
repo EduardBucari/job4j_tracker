8c97be4296aa52afd1dc5227cd64011292f5c7e5
Eduard Bucari <eduard.bucari@gmail.com>
2021-01-14 22:07:04 +0300

    PalindromeChecker

---
+++ src/main/java/ru/job4j/endavaTest/PalindromeChecker.java
@@ -0,0 +1,29 @@
+package ru.job4j.endavaTest;
+
+import java.util.*;
+// add additional imports if needed
+
+public class PalindromeChecker
+{
+    public List<Boolean> checkIfPalindromes(List<String> words) {
+        throw new UnsupportedOperationException();
+        //implement method here
+    }
+}
+
+/*
+Implement method checkIfPalindromes(List<String> words) so that it will
+return list of booleans which is result of test if word is palindrome*;
+
+*A palindrome is a word which reads the same backward as forward;
+
+Example:
+
+Input: [Madam, programmer, cIVic];
+
+Output: [true, false, true]
+
+Note: You should check if word is palindrome ignoring the case, so that
+for "cIVic" result should be true as well as for "civic" or "CIvic";
+
+ */
