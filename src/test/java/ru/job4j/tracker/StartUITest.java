82effd81dadf91692b412445209cd06c6ee53ba6
Eduard Bucari <eduard.bucari@gmail.com>
2021-01-16 01:30:54 +0300

    1.1. Тесты на StartUI. [#360995]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -176,4 +176,29 @@ public class StartUITest {
                         + "1. Exit" + ln + "=== Exit ===" + ln
         ));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+              new String[] {"1"}
+        );
+        Tracker tracker = new Tracker();
+        Item exit = tracker.add(new Item(1, "Exit"));
+        UserAction[] actions = {
+                new Exit(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                        + "0. Exit%n"
+                        + "Wrong input, you can select: 0 .. 0%n"
+                        + "Menu.%n"
+                        + "0. Exit%n"
+
+                )
+        ));
+
+    }
 }
