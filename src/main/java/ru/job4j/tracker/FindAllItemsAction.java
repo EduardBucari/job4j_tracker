0eac01dd50e141408e9c9af707335f4361f71e49
Eduard Bucari <eduard.bucari@gmail.com>
2021-02-17 19:39:40 +0300

    6. Изменить программу Tracker из 2-го модуля [#360954](6)

--- src/main/java/ru/job4j/tracker/FindAllItemsAction.java
+++ src/main/java/ru/job4j/tracker/FindAllItemsAction.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindAllItemsAction implements UserAction {
     private final Output out;
     public FindAllItemsAction(Output out) {
@@ -9,13 +12,17 @@ public class FindAllItemsAction implements UserAction {
     public String name() {
         return "Show all items";
     }
-
+    //заменим массивы на java.util.ArrayList.
     @Override
     public boolean execute(Input input, Tracker tracker) {
         out.println("=== Show all items ===");
-        Item[] rsl = tracker.findAll();
-        for (int i = 0; i < rsl.length; i++) {
-            out.println(rsl[i]);
+     //   Item[] rsl = tracker.findAll();
+     //   for (int i = 0; i < rsl.length; i++) {
+     //       out.println(rsl[i]);
+        List<Item> rsl = tracker.findAll();
+        if (rsl.size() > 0) {
+            for (Item i : rsl)
+                out.println(i);
         }
         return true;
     }
--- src/main/java/ru/job4j/tracker/FindItemsByNameAction.java
+++ src/main/java/ru/job4j/tracker/FindItemsByNameAction.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FindItemsByNameAction implements UserAction {
     private final Output out;
     public FindItemsByNameAction(Output out) {
@@ -10,14 +13,19 @@ public class FindItemsByNameAction implements UserAction {
         return "Find items by name";
     }
 
+     // заменим массивы на java.util.ArrayList.
     @Override
     public boolean execute(Input input, Tracker tracker) {
         out.println("=== Find items by name ===");
         String orderNames = input.askStr("Enter order names: ");
-        Item[] rsl = tracker.findByName(orderNames);
-        if (rsl.length > 0) {
-            for (int i = 0; i < rsl.length; i++) {
-                out.println(rsl[i]);
+     //   Item[] rsl = tracker.findByName(orderNames);
+     //   if (rsl.length > 0) {
+     //       for (int i = 0; i < rsl.length; i++) {
+     //           out.println(rsl[i]);
+        List<Item> rsl = tracker.findByName(orderNames);
+        if (rsl.size() > 0) {
+            for (Item i : rsl) {
+                out.println(i);
             }
         } else {
             out.println("Заявки с таким именем не найдены");
