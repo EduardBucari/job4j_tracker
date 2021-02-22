f6ba9b6e0c4e8341f688a1f9901c6b3c68921eff
Eduard Bucari <eduard.bucari@gmail.com>
2021-02-23 01:09:39 +0300

    1. Паспорт и жители [#360958]

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    // Метод add вернет true, если пользователя еще нет в HashMap. Если он уже есть, то вернуть false.
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+        //Для решения этой задачи потребуются методы Map.containsKey(key) и Map.get(key).
+        if (!citizens.containsKey("")) {
+            return true;
+        } else {
+            citizens.put(citizen.getPassport(), citizen);
+        }
+        return rsl;
+    }
+
+    public Citizen get(String passport) {
+        return null;
+    }
+}
