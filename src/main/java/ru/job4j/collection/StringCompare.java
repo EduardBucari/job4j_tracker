4591a115e2ba1a1c05ef8d7317a5d354a9642232
Eduard Bucari <eduard.bucari@gmail.com>
2021-03-11 17:58:56 +0300

    3. Компаратор для строк [#360943]

---
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,33 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+
+// Задача - Реализуйте класс ru.job4j.collection.StringCompare.
+// Необходимо реализовать поэлементное сравнение двух списков,
+// т.е. сравниваем элементы двух списков, находящихся на одних и тех же позициях
+// (под одним и тем же индексом). Сравнение в лексикографическом порядке.
+// В этом задании нельзя использовать метод String.compareTo.
+// Нужно использовать:
+// String.charAt(int index);
+// Integer.compare(int left, int right);
+// Character.compare(char left, char right);
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+
+        int leftLength = left.length();
+        int rightLength = right.length();
+        int result = 0;
+        int wordSize = Math.min(leftLength, rightLength);
+
+        for (int i = 0; i < wordSize; i++) {
+            result = Character.compare(left.charAt(i), right.charAt(i));
+            if (result != 0) {
+                return result;
+            }
+        }
+        return Integer.compare(leftLength, rightLength);
+    }
+}
