394961cd66dad217e4c738fd728c6ab872819a19
Eduard Bucari <eduard.bucari@gmail.com>
2021-03-29 01:16:10 +0300

    2. Функциональный интерфейс [#360776](4)

--- src/main/java/ru/job4j/function/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -31,22 +31,23 @@ public class FunctionalInterfaces {
         map.put(6, "six");
         map.put(7, "seven");
 
-        BiPredicate<Integer, String> biPred = (i, s) -> ;
+        BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || map.get(i).length() == 4;
         for (Integer i : map.keySet()) {
             /*
                 Замените проверку в if() на использование BiPredicate, он объявлен выше, требуется его реализовать.
+             (условие из if переносим в BiPredicate, а в if вызываем (biPred.test(i, map.get(i)))
              */
-            if (i % 2 == 0 || map.get(i).length() == 4) {
+          //  if (i % 2 == 0 || map.get(i).length() == 4) {
+            if (biPred.test(i, map.get(i))) {
                 System.out.println("key: " + i + " value: " + map.get(i));
             }
         }
         /*
             Заменить создание ArrayList из значений Map на Supplier, объявлен ниже, требуется его реализовать.
          */
-        Supplier<List<String>> sup = () -> new Map<>(map);
-        Map<> map1 = sup.get();
-
-        List<String> strings = new ArrayList<>(map.values());
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+        List<String> strings = sup.get();
+      //  List<String> strings = new ArrayList<>(map.values());
 
         Consumer<String> con = (s) -> System.out.println(s);
         Function<String, String> func = s -> s.toUpperCase();
@@ -56,7 +57,7 @@ public class FunctionalInterfaces {
                 Заменить преобразование строк к строкам в верхнем регистре с помощью Function
                 Необходимое объявлено выше, требуется их реализовать.
             */
-            System.out.println(s);
+            System.out.println(s.toUpperCase());
         }
     }
 }
