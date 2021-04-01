bd7f04214388b937286520486af5f760afa9ff94
Eduard Bucari <eduard.bucari@gmail.com>
2021-04-02 01:55:35 +0300

    4. Зона видимости в лямбда-выражениях и исключения [#360774]

---
+++ src/main/java/ru/job4j/lambda/ScopeInside.java
@@ -0,0 +1,26 @@
+package ru.job4j.lambda;
+
+import java.util.function.Supplier;
+
+/**
+ * Ниже приведен код, который складывает числа массива.
+ * Код не компилируется.
+ * Задача - Исправьть ошибку.
+ */
+public class ScopeInside {
+    public static void main(String[] args) {
+        int[] number = {1, 2, 3};
+        int total = 0;
+        for (int i = 0; i < number.length; i++) {
+            int num = i;
+            total = add(
+                    () -> total + num
+            );
+        }
+        System.out.println(total);
+    }
+
+    private static Integer add(Supplier<Integer> calc) {
+        return calc.get();
+    }
+}
