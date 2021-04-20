d230a6fbb9a7c2f4d8c68d288807456057443c91
Eduard Bucari <eduard.bucari@gmail.com>
2021-04-20 03:02:35 +0300

    3. Optional в банковских переводах. [#360798](2)

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -15,7 +15,14 @@ import java.util.Set;
  *    Ваша задача - переписать эти методы поиска студента и предмета (по аккаунту и имени),
  *    чтобы они возвращали тип Optional, по умолчанию нужно возвращать Optional.empty().
  *
- *  Проверить исправленный код готовыми тестами CollegeTest (они написаны уже на Optional).
+ * 3. Объект Optional предупреждает программиста, что метод может вернуть null
+ *    и нужно добавить проверку, что объект не null-ссылка.
+ *    Чтобы проверить, что объект не null используйте метод
+ *    if (opt.isPresent()) {
+ *    Чтобы получить значение этого объекта, используйте метод
+ *    opt.get()
+ *
+ * 4. Проверить исправленный код готовыми тестами CollegeTest (они написаны уже на Optional).
  */
 public class College {
     private final Map<Student, Set<Subject>> students;
@@ -37,9 +44,9 @@ public class College {
 
     public Optional<Subject> findBySubjectName(String account, String name) {
         Optional<Subject> rsl = Optional.empty();
-        Student s = findByAccount(account);
-        if (s != null) {
-            Set<Subject> subjects = students.get(s);
+       Optional<Student> s = findByAccount(account);
+        if (s.isPresent()) {
+            Set<Subject> subjects = students.get(s.get());
             for (Subject subj : subjects) {
                 if (subj.getName().equals(name)) {
                     rsl = Optional.of(subj);
