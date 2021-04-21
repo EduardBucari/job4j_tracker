0a13720b3e28848a6129570acc9d3158e5606a58
Eduard Bucari <eduard.bucari@gmail.com>
2021-04-22 01:46:13 +0300

    2. Комбинированный компаратор [#360946]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -6,60 +6,10 @@ import java.util.Comparator;
 import java.util.List;
 
 import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class JobTest {
 
-    // Тесты сортировки по имени(по возрастанию и по убыванию):
-
-    @Test
-    public void compareByNameAscending() {
-        List<Job> jobs = List.of(
-                new Job("ccc", 3),
-                new Job("aaa", 1),
-                new Job("bbb", 2)
-        );
-        jobs.sort(new JobSortByNameAscending());
-        assertThat(jobs.get(0).getName(), is("aaa"));
-    }
-
-    @Test
-    public void compareByNameDescending() {
-        List<Job> jobs = List.of(
-                new Job("ccc", 3),
-                new Job("aaa", 1),
-                new Job("bbb", 2)
-        );
-        jobs.sort(new JobSortByNameDescending());
-        assertThat(jobs.get(0).getName(), is("ccc"));
-    }
-
-    // Тесты сортировки по приоритету (по возрастанию и по убыванию):
-    @Test
-    public void compareByPriorityAscending() {
-        List<Job> jobs = List.of(
-                new Job("ccc", 3),
-                new Job("aaa", 1),
-                new Job("bbb", 2)
-        );
-        jobs.sort(new JobSortByPriorityAscending());
-        assertThat(jobs.get(0).getPriority(), is(1));
-    }
-
-    @Test
-    public void compareByPriorityDescending() {
-        List<Job> jobs = List.of(
-                new Job("ccc", 3),
-                new Job("aaa", 1),
-                new Job("bbb", 2)
-        );
-        jobs.sort(new JobSortByPriorityDescending());
-        assertThat(jobs.get(0).getPriority(), is(3));
-    }
-
-
-    
     //Тест для комбинированного компаратора.
     // Протестировать работу комбинированного компаратора так,
     // чтобы отрабатывали обе его части - для этого первый компаратор должен вернуть 0,
