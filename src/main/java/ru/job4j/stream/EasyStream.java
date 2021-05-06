cfd006054bb8e562dce54ef60bfd47bfc41bc564
Eduard Bucari <eduard.bucari@gmail.com>
2021-05-07 02:44:52 +0300

    EasyStream [#401629](2)

--- src/main/java/ru/job4j/stream/EasyStream.java
+++ src/main/java/ru/job4j/stream/EasyStream.java
@@ -1,10 +1,9 @@
 package ru.job4j.stream;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * В этом задании Вам нужно реализовать каркас упрощенного Stream API.
@@ -14,22 +13,58 @@ import java.util.stream.Stream;
  * filter - фильтрует поток элементов.
  * collect - собирает все элементы из source по заданным условиям map и filter.
  *
+ * В этом задании нужно использовать шаблон Builder.
  * Проверьте реализацию тестами.
  */
 public class EasyStream {
+    private final List<Integer> source;
+
+    public EasyStream(List<Integer> source) {
+        this.source = source;
+    }
+
+    /**
+     * метод of - получает исходные данные.
+     * @param source - Список данных целочисленного типа.
+     * @return - Объект типа EasyStream.
+     */
     public static EasyStream of(List<Integer> source) {
-        throw new UnsupportedOperationException();
+        return new EasyStream(source);
     }
 
+    /**
+     * метод map - преобразует число в другое число.
+     * @param fun - Функция преобразования Integer в Integer.
+     * @return - Объект типа EasyStream.
+     */
     public EasyStream map(Function<Integer, Integer> fun) {
-        throw new UnsupportedOperationException();
+        List<Integer> list = new ArrayList<>();
+        for (Integer element : source) {
+            list.add(fun.apply(element));
+        }
+        return new EasyStream(list);
     }
 
+    /**
+     * метод filter - фильтрует поток элементов.
+     * @param fun - Предикат целочисленного типа.
+     * @return - Объект типа EasyStream.
+     */
     public EasyStream filter(Predicate<Integer> fun) {
-        throw new UnsupportedOperationException();
+        List<Integer> list = new ArrayList<>();
+        for (Integer element : source) {
+            if (fun.test(element)) {
+                list.add(element);
+            }
+        }
+        return new EasyStream(list);
     }
 
+    /**
+     * метод collect - собирает все элементы из source по заданным условиям map и filter.
+     * @return - Коллекцию ArrayList с числами целочисленного типа.
+     */
     public List<Integer> collect() {
-        throw new UnsupportedOperationException();
+        return new ArrayList<>(source);
     }
 }
